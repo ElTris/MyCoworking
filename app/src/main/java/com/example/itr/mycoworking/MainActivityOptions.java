a043724209b62a92325184253b4a6b538ef8f05b
ElTris <tristanromero026@gmail.com>
2018-12-08 15:15:32 -0600

    finalizando diseños

--- app/src/main/java/com/example/itr/mycoworking/MainActivityOptions.java
+++ app/src/main/java/com/example/itr/mycoworking/MainActivityOptions.java
@@ -71,18 +71,13 @@ public class MainActivityOptions extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.Descricpion) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.historial) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.editar) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.acerca_de) {
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
